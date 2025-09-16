646abd73cca1fb1012f8e99ee49a1733d40ef6de
andreijeman <andrei.jeman3@gmail.com>
2025-09-16 10:52:31 +0300

    Implement Tcp Broker

---
+++ DistributedSystem/src/DistributedSystem.Broker/BrokerArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace DistributedSystem.Broker;
+
+public class BrokerArgs
+{
+    public int IpAddress { get; init; }
+    
+    public int PublisherPort { get; init; }
+    public int PublisherMaxConnections { get; init; }
+    
+    public int SubscriberPort { get; init; }
+    public int SubscriberMaxConnections { get; init; }
+}
