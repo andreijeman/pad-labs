0692808d29f9c6a9a84132587f5dcede5a508661
andreijeman <andrei.jeman3@gmail.com>
2025-09-16 11:07:01 +0300

    Implement Socket Postman

---
+++ DistributedSystem/src/DistributedSystem.Network/ICodec.cs
@@ -0,0 +1,7 @@
+﻿namespace DistributedSystem.Network;
+
+public interface ICodec<TPacket>
+{
+    public byte[] Pack(TPacket packet);
+    public TPacket Unpack(byte[] data, int index, int count);
+}
